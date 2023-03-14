b12243ab7c53e03a284082064e5292b6761ec66c
Kica Ronald Okello <okello.ronald045@gmail.com>
2023-03-14 15:58:47 +0300

    Admin dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
