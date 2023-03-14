43b44efe63dfd45ed1ca02908a1ec2fe281b33a1
Kica Ronald Okello <okello.ronald045@gmail.com>
2023-03-14 15:03:54 +0300

    Create login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
